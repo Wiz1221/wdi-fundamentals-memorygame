d3e84e72fbd346367d2a71e16490c752d70aeb58
PGY <PGY@PGYs-MacBook-Air.local>
2017-05-11 16:06:40 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardsInPlay = [];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length===2){
+	console.log("correct length")
+}
+if (cardsInPlay[1]===cardsInPlay[0]) {
+	alert("You found a match!");
+}else{
+	alert("try again");
+}
+for (var i = 0; i < cardsInPlay.length; i++) {
+	console.log("User flipped " + cardsInPlay[i]);
+}
